cda4c4c2f4348d970ccfcd5efccc08cc7920a972
michele.sama@gmail.com <michele.sama@gmail.com@7979b95b-94e8-d172-6193-de21f142e1ad>
2011-09-14 16:40:40 +0000

    fixed archiving

--- trunk/BusStopBuddy/BusStop.h
+++ trunk/BusStopBuddy/BusStop.h
@@ -9,20 +9,13 @@
 #import <Foundation/Foundation.h>
 
 
-@interface BusStop : NSObject <NSCoding> {
-
-    NSString *_stopId;
-    NSString *_stopName;
-    NSString *_stopLetter;
-    BOOL _favourite;
-    NSDate *_lastUsage;
-}
+@interface BusStop : NSObject <NSCoding, NSCopying>
 
 @property (nonatomic, retain) NSString *stopId;
 @property (nonatomic, retain) NSString *stopName;
 @property (nonatomic, retain) NSString *stopLetter;
 @property (nonatomic) BOOL favourite;
-@property (nonatomic, retain) NSDate *lastUsage;
+
 
 - (id) initWithId:(NSString *)stopId andName:(NSString *)stopName andLetter:(NSString *)stopLetter;
 
--- trunk/BusStopBuddy/BusStop.m
+++ trunk/BusStopBuddy/BusStop.m
@@ -11,11 +11,10 @@
 
 @implementation BusStop
 
-@synthesize stopId=_stopId;
-@synthesize stopName=_stopName;
-@synthesize stopLetter=_stopLetter;
-@synthesize favourite=_favourite;
-@synthesize lastUsage=_lastUsage;
+@synthesize stopId = _stopId;
+@synthesize stopName = _stopName;
+@synthesize stopLetter = _stopLetter;
+@synthesize favourite = _favourite;
 
 - (id) initWithId:(NSString *)stopId andName:(NSString *)stopName andLetter:(NSString *)stopLetter {
     self = [super init];
@@ -40,7 +39,6 @@
     [encoder encodeObject: _stopName forKey:@"objStopName"];
     [encoder encodeObject: _stopLetter forKey:@"objStopLetter"];    
     [encoder encodeBool:self.favourite forKey:@"objFavourite"];
-    [encoder encodeObject:_lastUsage forKey:@"objLastUsage"];
 }
 
 - (id)initWithCoder:(NSCoder *)decoder {
@@ -49,10 +47,18 @@
         self.stopName = [decoder decodeObjectForKey:@"objStopName"];
         self.stopLetter = [decoder decodeObjectForKey:@"objStopLetter"];
         self.favourite = [decoder decodeBoolForKey:@"objFavourite"];
-        self.lastUsage = [decoder decodeObjectForKey:@"objLastUsage"];
     }
     return self;
 }
 
+- (id) copyWithZone:(NSZone *)zone{
+    BusStop *object = [[[self class] allocWithZone:zone] init];
+    object->_stopId = [_stopId copyWithZone:zone];
+    object->_stopName = [_stopName copyWithZone:zone];
+    object->_stopLetter = [_stopLetter copyWithZone:zone];
+    object->_favourite = _favourite;
+    return object;
+}
+ 
 
 @end
